3bac60befdcfba49c88ea3913a7b41d563c60def
Rajeev KARUVATH <rajeevkaruvath@gmail.com>
2020-04-12 20:49:56 +0530

    BestIndex change algoritm

--- hackerearth/BestIndex.py
+++ hackerearth/BestIndex.py
@@ -2,24 +2,37 @@ from datetime import datetime
 start = datetime.now()
 noOfCases = int(input())
 a = [ int(x) for x in input().split(" ") ]
-print('Parsing Time: ', datetime.now() - start)
+# print('Parsing Time: ', datetime.now() - start)
 listOfSums = []
+from bisect import bisect_left
+
+def binary_search_custom(a, x, lo=0, hi=None):
+    hi = hi if hi is not None else len(a)
+    pos = bisect_left(a, x, lo, hi)
+    if pos != hi and a[pos] == x:
+        return a[pos]
+    return a[pos-1] if pos != hi and a[pos] >= x else -1
+
+sumOfFirst = [int((i * (i + 1)) / 2) for i in range(0,noOfCases) if i > 0]
+
+def getMaxIndex(lenOfArray):
+    return binary_search_custom(sumOfFirst,lenOfArray)
+
+
+# print(sumOfFirst[0:10])
 
 for initIndex in range(0,noOfCases):
-    print("current index: ",initIndex,end="")
+    # print("current array",initIndex)
     start = datetime.now()
-    sumTotal = 0
-    for counter in range(0,noOfCases):
-        sliceEnd = initIndex + counter + 1
-        # print(initIndex,":",sliceEnd)
-        if sliceEnd > noOfCases:
-            break
-        t = a[initIndex:sliceEnd]
-        # print(t)
-        sumTotal += sum(t)
-        initIndex = sliceEnd
-    print('Done in ', datetime.now() - start)
+    nearestIndex = getMaxIndex(len(a))
+    # print("nearest index",nearestIndex-1)
+    modified = a[:nearestIndex]
+    # print("modified",modified)
+    sumTotal = sum(modified)
+    # print("sum", sumTotal)
+    # print('Done in ', datetime.now() - start)
     listOfSums.append(sumTotal)
+    a.pop(0)
 start = datetime.now()
 print(max(listOfSums))
-print('max calculation : ', datetime.now() - start)
+# print('max calculation : ', datetime.now() - start)
